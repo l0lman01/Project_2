c743a54148c2c15e5da2efeb8978a1e0590ad85c
l0lman01 <leo.phamvu@gmail.com>
2021-11-05 00:16:31 +0100

    deplacement joueurs

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -5,7 +5,7 @@
 using namespace sf;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(1200, 900), "SFML Works");
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "SFML Works");
     sf::RectangleShape entity(sf::Vector2f(100.f, 100.f));
     Texture texture;
     Texture maptextur;
@@ -19,16 +19,35 @@ int main() {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
                 window.close();
-        }
 
+            //azerty
+            switch (event.key.code) {
+                //Haut
+            case sf::Keyboard::Z:
+                entity.move(0.f, -2.f);
+                break;
+                //Down
+            case sf::Keyboard::S:
+                entity.move(0.f, 2.f);
+                break;
+                //Droite
+            case sf::Keyboard::D:
+                entity.move(2.f, 0.f);
+                break;
+                //Gauche
+            case sf::Keyboard::Q:
+                entity.move(-2.f, 0.f);
+                break;
+            }
+        }
         window.clear();
 
         //window.draw(shape);
-        window.draw(sprite);
+        //window.draw(sprite);
         window.draw(T.load_Level(maptextur));
-        //window.draw(entity);
+        window.draw(entity);
         window.display();
     }
     return 0;
