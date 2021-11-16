57f32c2adee8c72acee9254903bdf1eede174395
Luna <51327769+lunarflarea@users.noreply.github.com>
2021-11-16 10:32:12 +0100

    Finished enemy's development
    Finished 4th movement
    Finished animation

--- Project2/enemy.cpp
+++ Project2/enemy.cpp
@@ -10,7 +10,7 @@ using namespace sf;
 Enemy::Enemy(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed):
 		anime(texture, imageCount, switchTime) {
 		this->speed = speed;
-		row = 4;
+		row = 6;
 		isMoving = false;
 		body.setSize(sf::Vector2f(100.f, 100.f));
 		body.setPosition(100.f, 100.f);
@@ -29,15 +29,15 @@ void Enemy::Update(float deltaTime) {
 	Vector2f chemin;
 	std::vector<Vector2f> data = { 
 		Vector2f(390,20),
+		Vector2f(390,20),
+		Vector2f(435,20),
 		Vector2f(435,20),
 		Vector2f(435,70),
-		Vector2f(390,70)
-		/*
-			Créer une clock
-			Quand l'ennemi est arrivé à destination, faire arriver getelapsetime à 1
-			Quand la clock est à 1, relancer le mouvement
-			utiliser getelapsetime = 1 (pour 1 seconde)
-		*/ 
+		Vector2f(435,70),
+		Vector2f(390,70),
+		Vector2f(390,70),
+		Vector2f(390,20),
+		Vector2f(390,20)
 	};
 
 	accum += deltaTime;
@@ -46,16 +46,35 @@ void Enemy::Update(float deltaTime) {
 	{
 		accum = 0;
 		segment = segment + 1;
-	}
 
-	if (segment >= 3)
-	{
-		segment = 0;
+		if (segment >= 8)
+		{
+			segment = 0;
+		}
+
+		switch (segment) {
+		case 0:
+		case 1:
+			row = 6;
+			break;
+		case 2:
+		case 3:
+			row = 4;
+			break;
+		case 4:
+		case 5:
+			row = 5;
+			break;
+		case 6:
+		case 7:
+			row = 7;
+			break;
+		}
 	}
 
 	Vector2f mouvement = interp2d(data[segment], data[segment+1], accum);
 	body.setPosition(mouvement);
 
 	body.setTextureRect(anime.uvRect);
-	anime.Update(row, deltaTime, 0, 3);
+	anime.Update(row, deltaTime, 0, 2);
 };
